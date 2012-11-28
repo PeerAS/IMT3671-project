96423b74f1e54cd64a53507dd49a7c00a2c4161a
Peer Andreas Stange <peerandreas@live.com>
2012-11-29 00:31:34 +0100

    Small changes
    Did some small changes to the main menu. Also made a second mockup of
    DrinkingTime.xaml

--- Mobile_project/MainPage.xaml.cs
+++ Mobile_project/MainPage.xaml.cs
@@ -19,37 +19,25 @@ namespace Mobile_project
     {
         private string[] names;
         private double[] weights;
+        static int[] registered;
+        
         const int MAX_USERS = 3;
-        private int i;
+        private int arrayIterator;
         // Constructor
         public MainPage()
         {
+            
             InitializeComponent();
             names = new string[MAX_USERS]{"Register New Person", "Register New Person", "Register New Person"};
-            weights = new double[MAX_USERS];
-            i = 0;
+            weights = new double[MAX_USERS]{0, 0, 0};
+            registered = new int[MAX_USERS]{ 0, 0, 0 };
+
+            arrayIterator = 0;
             this.DataContext = App._appViewModel;
 
             // Sample code to localize the ApplicationBar
             //BuildLocalizedApplicationBar();
-            if (App._appViewModel.person.Count() != 0)
-            {
-                var personname = from personTable in App._appViewModel.person
-                                 where personTable.personID <= MAX_USERS
-                                 select new { Name = personTable.personName, weight = personTable.personWeight };
-
-                
-                foreach (var item in personname)
-                {
-                    names[i] = item.Name;
-                    weights[i] = item.weight;
-                    i++;
-                }
-
-                Person_1.Content = names[0];
-                Person_2.Content = names[1];
-                Person_3.Content = names[2];
-            }
+           
         }
 
         private void Other_Person_Click(object sender, RoutedEventArgs e)
@@ -59,18 +47,82 @@ namespace Mobile_project
 
         private void Person_1_Click(object sender, RoutedEventArgs e)
         {
-            double temp_weight = weights[1];
-            this.NavigationService.Navigate(new Uri("/DrinkingTime.xaml?weight="+temp_weight, UriKind.Relative));
+            var person1 = from personTable in App._appViewModel.person
+                          where personTable.personID == 1
+                          select new { Name = personTable.personName, weight = personTable.personWeight };
+
+            if (person1.Count() == 0)
+            {
+                this.NavigationService.Navigate(new Uri("/Addperson.xaml", UriKind.Relative));
+            }
+            else if (person1.Count() == 1)
+            {
+                double temp_weight = weights[0];
+                this.NavigationService.Navigate(new Uri("/DrinkingTime.xaml?weight=" + temp_weight, UriKind.Relative));
+            }
+           
         }
 
         private void Person_2_Click(object sender, RoutedEventArgs e)
         {
-            this.NavigationService.Navigate(new Uri("/DrinkingTime.xaml", UriKind.Relative));
+            var person2 = from personTable in App._appViewModel.person
+                          where personTable.personID == 2
+                          select new { Name = personTable.personName, weight = personTable.personWeight };
+
+            if (person2.Count() == 0)
+            {
+                this.NavigationService.Navigate(new Uri("/AddPerson.xaml", UriKind.Relative));   
+            }
+            else if (person2.Count() == 1)
+            {
+                double temp_weight = weights[1];
+                this.NavigationService.Navigate(new Uri("/DrinkingTime.xaml?weight=" + temp_weight, UriKind.Relative));
+            }
+
         }
 
         private void Person_3_Click(object sender, RoutedEventArgs e)
         {
-            this.NavigationService.Navigate(new Uri("/AddPerson.xaml", UriKind.Relative));
+            var person3 = from personTable in App._appViewModel.person
+                          where personTable.personID == 3
+                          select new { Name = personTable.personName, weight = personTable.personWeight };
+
+            if (person3.Count() == 0)
+            {
+                this.NavigationService.Navigate(new Uri("/AddPerson.xaml", UriKind.Relative));
+            }
+            else if (person3.Count() == 1)
+            {
+                double temp_weight = weights[2];
+                this.NavigationService.Navigate(new Uri("/DrinkingTime.xaml?weight="+ temp_weight, UriKind.Relative));
+            }
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            arrayIterator = 0;
+
+            if (App._appViewModel.person.Count() != 0)
+            {
+                var personname = from personTable in App._appViewModel.person   //select from the observable collection person
+                                 where personTable.personID <= MAX_USERS        //and put it in a var.
+                                 select new { Name = personTable.personName, weight = personTable.personWeight };
+
+
+                foreach (var item in personname)
+                {
+                    names[arrayIterator] = item.Name;
+                    weights[arrayIterator] = item.weight;
+                    arrayIterator++;
+                }      
+            }
+
+            Person_1.Content = names[0];
+            Person_2.Content = names[1];
+            Person_3.Content = names[2];
+
+            
+            base.OnNavigatedTo(e);
         }
 
         
